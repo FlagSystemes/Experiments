da4dcae97b94d2204033440027e6836cbdf016ec
Simon Cropp <simon.cropp@gmail.com>
2013-03-16 19:54:25 +1100

    add cecil issue

---
+++ CecilDebugIssue/CecilDebugIssue/NonAsyncTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class NonAsyncTest
+{
+    [Test]
+    public void TestAsynch()
+    {
+        AsyncDebuggerTest();
+    }
+
+    public void AsyncDebuggerTest()
+    {
+        var test = getTestString();
+        //set breakpoint on next line and look at 'test' variable in debugger
+        var test2 = test + test;
+    }
+
+    private string getTestString()
+    {
+        return "test";
+    }
+}
---
+++ CecilDebugIssue/WithNoCecil/AsyncTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+
+[TestFixture]
+public class AsyncTest
+{
+    [Test]
+    public void TestAsynch()
+    {
+        AsyncDebuggerTest();
+    }
+
+    public async void AsyncDebuggerTest()
+    {
+        var test = await getTestString();
+        //set breakpoint on next line and look at 'test' variable in debugger
+        var test2 = test + test;
+    }
+
+    private async Task<string> getTestString()
+    {
+        return "test";
+    }
+}
